b645be140717b3a06da212db081d40da9f9c86bd
Kavya <2k22cse077@kiot.ac.in>
2026-03-18 11:42:35 +0530

    added auth service and controller

---
+++ Backend/src/main/java/com/example/RMS/controller/AuthController.java
@@ -0,0 +1,36 @@
+package com.example.RMS.controller;
+
+import com.example.RMS.dto.Response.AuthResponse;
+import com.example.RMS.dto.Request.LoginRequest;
+import com.example.RMS.dto.Request.UserRequest;
+import com.example.RMS.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        try {
+            return ResponseEntity.ok(authService.login(request));
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(401).build();
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody UserRequest request) {
+        try {
+            return ResponseEntity.ok(authService.register(request));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
